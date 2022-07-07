d40981234c6776df38768c32bf2be69afca1ccc5
BeainaT <tania.df@hotmail.it>
2022-07-07 17:15:02 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,36 @@
-<header></header>
+<header>
+    <div class="header_top">
+        <div class="top container">
+            <span>
+                <a href="http://">dc power℠ visa®</a>
+            </span>
+            <span>
+                additional dc sites
+            </span>
+        </div>
+    </div>
+    <div class="header_bottom">
+        <div class="bottom container">
+            <div class="logo">
+                <img src={{asset('img/dc-logo.png')}} alt="dc logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">characters</a></li>
+                    <li><a href="">comics</a></li>
+                    <li><a href="">movies</a></li>
+                    <li><a href="">tv</a></li>
+                    <li><a href="">games</a></li>
+                    <li><a href="">collectibles</a></li>
+                    <li><a href="">videos</a></li>
+                    <li><a href="">fans</a></li>
+                    <li><a href="">characters</a></li>
+                    <li><a href="">shop</a></li>
+                </ul>
+            </nav>
+            <span>
+                <input type="text" placeholder="Search"><i class="fa-solid fa-magnifying-glass"></i>
+            </span>
+        </div>
+    </div>
+</header>
